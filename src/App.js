619eea821fe8ec39a2ea77437dfc8d66d804bb8c
ironlionm4n <mikailsmiller@gmail.com>
2022-06-02 19:41:48 -0400

    error modal and new user handling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-import React from "react";
-import AddUser from "./Components/Users/AddUser";
+import React, { useState } from 'react'
+import AddUser from './Components/Users/AddUser'
+import UsersList from './Components/Users/UsersList'
 
-function App() {
+function App () {
+  const [users, setUsersList] = useState([])
+  const handleAddUser = (userName, userAge) => {
+    setUsersList(prevState => {
+      return [...prevState, {name: userName, age: userAge, id: Math.random().toString()}]
+    })
+  }
   return (
-    <div >
-      <AddUser />
+    <div>
+      <AddUser onAddUser={handleAddUser}/>
+      <UsersList users={users} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,22 +1,76 @@
-import React from 'react'
+import React, { useState, Fragment } from 'react'
+import Button from '../UI/Button'
 import Card from '../UI/Card'
+import ErrorModal from '../UI/ErrorModal'
 import classes from './AddUser.module.css'
 
 const AddUser = props => {
+  const [enteredUsername, setEnteredUsername] = useState('')
+  const [enteredAge, setEnteredAge] = useState('')
+  const [errorModal, setModalOn] = useState({title: '', age: '', show: false})
+
   const handleAddUser = event => {
     event.preventDefault()
+
+    if (
+      enteredUsername.trim().length === 0 ||
+      enteredAge.trim().length === 0 ||
+      +enteredAge < 0
+    ) {
+      
+      setModalOn({
+        title: 'Invalid Input',
+        message: 'Be Sure Name & Age Are Valid',
+        show: true
+      })
+      
+      setEnteredAge('')
+      setEnteredUsername('')
+      return
+    }
+
+    props.onAddUser(enteredUsername, enteredAge);
+    setEnteredUsername('')
+    setEnteredAge('')
+  }
+
+  const handleUsernameChange = event => {
+    setEnteredUsername(event.target.value)
+  }
+
+  const handleAgeChange = event => {
+    setEnteredAge(event.target.value)
+  }
+
+  const handleModalOff = () => {
+    setModalOn(prevState => {
+      return {title: '', age: '', show: false}
+    })
   }
 
   return (
+    <Fragment>
+    {errorModal.show && <ErrorModal title={errorModal.title} message={errorModal.message} actions="Stop" handleModalOff={handleModalOff}/>}
     <Card className={classes.input}>
       <form onSubmit={handleAddUser}>
         <label htmlFor='username'>Username</label>
-        <input id='username' type='text' />
+        <input
+          id='username'
+          type='text'
+          onChange={handleUsernameChange}
+          value={enteredUsername}
+        />
         <label htmlFor='age'>Age</label>
-        <input id='age' type='number' />
-        <button type='submit'>+ User</button>
+        <input
+          id='age'
+          type='number'
+          onChange={handleAgeChange}
+          value={enteredAge}
+        />
+        <Button type='submit'>+ User</Button>
       </form>
     </Card>
+    </Fragment>
   )
 }
 
