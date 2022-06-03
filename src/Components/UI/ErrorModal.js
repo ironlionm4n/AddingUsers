05024e7af4ad44ad5c10b2e42c93d3b8cd14f0a6
ironlionm4n <mikailsmiller@gmail.com>
2022-06-03 07:49:21 -0400

    Input values read via refs

--- src/Components/UI/ErrorModal.js
+++ src/Components/UI/ErrorModal.js
@@ -1,24 +1,45 @@
-import React from 'react'
+import React, { Fragment } from 'react'
+import * as ReactDOM from 'react-dom'
 import Card from './Card'
 import Button from './Button'
 import classes from './ErrorModal.module.css'
 
+const Backdrop = props => {
+  return <div className={classes.backdrop} onClick={props.handleModalOff}></div>
+}
+
+const ModalOverlay = props => {
+  return (
+    <Card className={classes.modal}>
+      <header className={classes.header}>
+        <h2>{props.title}</h2>
+      </header>
+      <div className={classes.content}>
+        <p>{props.message}</p>
+      </div>
+      <footer className={classes.actions}>
+        <Button onClick={props.handleModalOff}>Understood</Button>
+      </footer>
+    </Card>
+  )
+}
+
 const ErrorModal = props => {
   return (
-    <div>
-      <div className={classes.backdrop}></div>
-      <Card className={classes.modal}>
-        <header className={classes.header}>
-          <h2>{props.title}</h2>
-        </header>
-        <div className={classes.content}>
-          <p>{props.message}</p>
-        </div>
-        <footer className={classes.actions}>
-          <Button onClick={props.handleModalOff}>Understood</Button>
-        </footer>
-      </Card>
-    </div>
+    <Fragment>
+      {ReactDOM.createPortal(
+        <Backdrop handleModalOff={props.handleModalOff} />,
+        document.getElementById('backdrop-root')
+      )}
+      {ReactDOM.createPortal(
+        <ModalOverlay
+          title={props.title}
+          message={props.message}
+          handleModalOff={props.handleModalOff}
+        />,
+        document.getElementById('overlay-root')
+      )}
+    </Fragment>
   )
 }
 
--- src/Components/Users/AddUser.js
+++ src/Components/Users/AddUser.js
@@ -1,75 +1,71 @@
-import React, { useState, Fragment } from 'react'
+import React, { useState, Fragment, useRef } from 'react'
 import Button from '../UI/Button'
 import Card from '../UI/Card'
 import ErrorModal from '../UI/ErrorModal'
 import classes from './AddUser.module.css'
 
 const AddUser = props => {
-  const [enteredUsername, setEnteredUsername] = useState('')
-  const [enteredAge, setEnteredAge] = useState('')
-  const [errorModal, setModalOn] = useState({title: '', age: '', show: false})
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
+  const [errorModal, setModalOn] = useState({ title: '', age: '', show: false })
 
   const handleAddUser = event => {
     event.preventDefault()
 
+    const enteredName = nameInputRef.current.value
+    const enteredUserAge = ageInputRef.current.value
+
     if (
-      enteredUsername.trim().length === 0 ||
-      enteredAge.trim().length === 0 ||
-      +enteredAge < 0
+      enteredName.trim().length === 0 ||
+      enteredUserAge.trim().length === 0 ||
+      +enteredUserAge < 1
     ) {
-      
       setModalOn({
         title: 'Invalid Input',
         message: 'Be Sure Name & Age Are Valid',
         show: true
       })
-      
-      setEnteredAge('')
-      setEnteredUsername('')
       return
     }
-
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername('')
-    setEnteredAge('')
-  }
-
-  const handleUsernameChange = event => {
-    setEnteredUsername(event.target.value)
-  }
-
-  const handleAgeChange = event => {
-    setEnteredAge(event.target.value)
+    props.onAddUser(enteredName, enteredUserAge)
+    nameInputRef.current.value = ''
+    ageInputRef.current.value = ''
   }
 
   const handleModalOff = () => {
     setModalOn(prevState => {
-      return {title: '', age: '', show: false}
+      return { title: '', message: '', show: false }
     })
   }
 
   return (
     <Fragment>
-    {errorModal.show && <ErrorModal title={errorModal.title} message={errorModal.message} actions="Stop" handleModalOff={handleModalOff}/>}
-    <Card className={classes.input}>
-      <form onSubmit={handleAddUser}>
-        <label htmlFor='username'>Username</label>
-        <input
-          id='username'
-          type='text'
-          onChange={handleUsernameChange}
-          value={enteredUsername}
+      {errorModal.show && (
+        <ErrorModal
+          title={errorModal.title}
+          message={errorModal.message}
+          actions='Stop'
+          handleModalOff={handleModalOff}
         />
-        <label htmlFor='age'>Age</label>
-        <input
-          id='age'
-          type='number'
-          onChange={handleAgeChange}
-          value={enteredAge}
-        />
-        <Button type='submit'>+ User</Button>
-      </form>
-    </Card>
+      )}
+      <Card className={classes.input}>
+        <form onSubmit={handleAddUser}>
+          <label htmlFor='username'>Username</label>
+          <input
+            id='username'
+            type='text'
+            ref={nameInputRef}
+          />
+          <label htmlFor='age'>Age</label>
+          <input
+            id='age'
+            type='number'
+            ref={ageInputRef}
+          />
+          <Button type='submit'>+ User</Button>
+        </form>
+      </Card>
     </Fragment>
   )
 }
